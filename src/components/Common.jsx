4995c2ef78329cc2d9123f3fe717daeb53573830
Virenk21 <viren.d222111@gmail.com>
2022-10-31 00:01:49 +0530

    added usestate hooks

--- src/components/Common.jsx
+++ src/components/Common.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Common = () => {
     return (
         <div>
-            <h1>useState hook</h1>
+            <h1>useState hook explained</h1>
         </div>
     );
 };
